772507bb0d715280c5798b196ee8c98e4fea6e1c
chenct <chenct@7adybird.com>
2016-09-17 07:14:54 +0000

    Fix required ivy version number.

--- ivy-xcdoc.el
+++ ivy-xcdoc.el
@@ -5,7 +5,7 @@
 ;; Author: C.T.Chen <chenct@7adybird.com>
 ;; URL: https://github.com/hex2010/emacs-ivy-xcdoc
 ;; Version: 0.0.1
-;; Package-Requires: ((ivy) (emacs "24.1"))
+;; Package-Requires: ((ivy "0.8.0") (emacs "24.1"))
 
 ;; This file is NOT part of GNU Emacs.
 
